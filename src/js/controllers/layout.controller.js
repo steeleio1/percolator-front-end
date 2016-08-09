80327e981636871c33cff39086443a3cbb6923f2
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-08-09 18:53:01 -0400

    Fixed bug in Sign In button dropdown

--- src/js/controllers/layout.controller.js
+++ src/js/controllers/layout.controller.js
@@ -6,21 +6,18 @@ function LayoutController () {
 	vm.login = false;
 // Adds the function to the vm object.
 	vm.showLogin = showLogin;
+	vm.cancelLogin = cancelLogin;
 
 // Defines the function and hoists to top of file.
 	function showLogin(){
-		// If vm.login is true, showLogin() toggles vm.login to false.
-		if (vm.login){
-			vm.login = false;
-		} else {
 			// If vm.login is false, showLogin() toggles vm.login to true, resuling in
 			// ngShow showing the dropdown.
 			vm.login = true;
-		}
 	}
 
-
-
+	function cancelLogin(){
+		vm.login = false;
+	}
 }
 
 LayoutController.$inject = [];
