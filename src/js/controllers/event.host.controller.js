177a1d530124aa0e85fe70239089ecd95349e860
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-08-11 17:32:00 -0400

    Added Invite New Contact and Invite My Contacts forms to event-host.tpl.html

--- src/js/controllers/event.host.controller.js
+++ src/js/controllers/event.host.controller.js
@@ -1,5 +1,30 @@
 function EventHostController () {
 
+	let vm = this;
+
+	vm.showInviteNew = showInviteNew;
+	vm.showInviteMyContacts = showInviteMyContacts;
+	vm.hideInviteNew = hideInviteNew;
+	vm.hideInviteMyContacts = hideInviteMyContacts;
+	vm.inviteNew = false;
+	vm.inviteMyContacts- false;
+
+	function showInviteNew() {
+		vm.inviteNew = true;
+	}
+
+	function showInviteMyContacts (){
+		vm.inviteMyContacts=true;
+	}
+
+	function hideInviteNew(){
+		vm.inviteNew = false;
+	}
+
+	function hideInviteMyContacts(){
+		vm.inviteMyContacts=false;
+	}
+
 }
 
 EventHostController.$inject = [];
