8a4c90b09ef99386c5ad641f8e913663f94d1366
Niclas Willems <niclas.willems@gmail.com>
2016-08-11 15:30:04 -0400

    Refactored server calls in create account and layout. WIP on host controller to view data.

--- src/js/controllers/host.controller.js
+++ src/js/controllers/host.controller.js
@@ -1,16 +1,18 @@
-function HostController ($state) {
+function HostController($state, $scope, $stateParams, $http, SERVER) {
 
-	let vm = this;
-	vm.active1 = true;
-	vm.active2 = false;
+    let vm = this;
+    vm.active1 = true;
+    vm.active2 = false;
 
-	init ();
+    init();
 
-  function init () {
-      $state.go('root.host.myEvents');
-  }
+    function init() {
+        $http.get(SERVER.URL + $stateParams.id).then((res) => {
+            $state.go('root.host.myEvents');
+        });
 
+    }
 }
 
-HostController.$inject = ['$state'];
+HostController.$inject = ['$state', '$scope', '$stateParams', '$http', 'SERVER'];
 export { HostController };
