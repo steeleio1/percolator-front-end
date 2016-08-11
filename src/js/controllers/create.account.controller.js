90bd071ec1b8d8d53995f4bfbc5a54f73749a2fa
Niclas Willems <niclas.willems@gmail.com>
2016-08-10 21:07:41 -0400

    Hooked up create account to backend. User data is being saved, but still need to hook up address table.  Added code for filepicker api and css for styling of filepicker button.

--- src/js/controllers/create.account.controller.js
+++ src/js/controllers/create.account.controller.js
@@ -1,6 +1,51 @@
-function CreateAccountController () {
+function CreateAccountController ($http, SERVER, $state) {
+
+  // Sets up this as vm.
+  let vm = this;
+
+  // Adds the function to the vm object.
+  vm.createAccount = createAccount;
+
+  function createAccount(user, address) {
+      console.log(user);
+      console.log(address);
+
+      // This line is for development code on localhost
+      // $http.post('http://localhost:3333/register', user).then( res => {
+      //   console.log(res);
+      //   if (res.status == 200) {
+      //           alert("200 OK");
+      //       }
+      // },
+      //     function errorCallback(res){
+      // 			if (res.status == 401) {
+      //         alert("401 ERROR!!!!!");
+      // 			} else if (res.status == 403) {
+      //         alert("403 Forbidden");
+      // 			}
+      //
+      //     });
+
+      // This line is for production code
+      // $http.post(SERVER.URL + 'register', user).then(function successCallback(res) {
+      //     console.log(res);
+      //     if (res.status == 200) {
+      //         alert("200 OK");
+      //         $cookies.put('access_token', res.data.access_token);
+      //         $state.go('root.host');
+			// 			}
+			// 	},
+      //     function errorCallback(res){
+			// 			if (res.status == 401) {
+      //         alert("401 ERROR!!!!!");
+			// 			} else if (res.status == 403) {
+      //         alert("403 Forbidden");
+			// 			}
+      //
+      //     });
+  };
 
 }
 
-CreateAccountController.$inject = [];
+CreateAccountController.$inject = ['$http', 'SERVER', '$state'];
 export { CreateAccountController };
