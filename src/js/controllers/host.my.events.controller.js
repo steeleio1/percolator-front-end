9ff48ea781511a536f6b4aa9d56d759511723e38
Niclas Willems <niclas.willems@gmail.com>
2016-08-12 23:46:11 -0400

    Working on event delete function.  Not working yet but commiting so I can go to bed.

--- src/js/controllers/host.my.events.controller.js
+++ src/js/controllers/host.my.events.controller.js
@@ -1,7 +1,11 @@
 function HostMyEventsController ($state, $scope, $http, SERVER, $cookies) {
 
+  // Sets up this as vm.
   let vm = this;
+  // Sets up variables
   vm.events = [];
+  // Adds the function to the vm object
+  vm.deleteEvent = deleteEvent;
 
   init();
 
@@ -15,6 +19,20 @@ function HostMyEventsController ($state, $scope, $http, SERVER, $cookies) {
         console.log(vm.events);
 			});
 	}
+
+  function deleteEvent(eventID) {
+    var result = confirm("Confirm delete of this event?");
+      if (result) {
+        console.log(eventID);
+        let token = $cookies.get('access_token');
+  			let config = {
+  				headers: { 'Authorization': `Bearer ${token}` }
+  			};
+  			$http.delete(SERVER.URL + 'events/' + eventID, config).then((res) => {
+          console.log('done');
+  			});
+      }
+  }
 
 }
 
