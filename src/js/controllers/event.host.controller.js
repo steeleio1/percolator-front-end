011b5908f7064b7215c9769071749b839226a13e
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-08-22 13:53:55 -0400

    Updated event.host.controller.js so that Event Date and Times are displayed correctly, updated host.event.tpl accordingly

--- src/js/controllers/event.host.controller.js
+++ src/js/controllers/event.host.controller.js
@@ -28,6 +28,8 @@ function EventHostController (MailService, WealthService, $state, $http, SERVER,
 	// May not need this here due to WE Report being generated on RSVP submit.
 	// vm.getWEReport = getWEReport;
 	vm.guestDetails = guestDetails;
+	vm.formatDate = formatDate;
+	vm.getTime =getTime;
 
 	init();
 
@@ -209,6 +211,52 @@ function emailGuest(egInfo, guestInstance){
 	function guestDetails(guestID) {
 		$location.url('host/guests/' + guestID);
 	}
+
+  function formatDate(d){
+    // let aa;
+    // let hours;
+    // let minutes;
+    let date = new Date(d)
+    var dd = date.getDate();
+    var mm = date.getMonth()+1;
+    var yyyy = date.getFullYear();
+    if(dd<10){dd='0'+dd}
+    if(mm<10){mm='0'+mm};
+    return d = mm+'/'+dd+'/'+yyyy;
+  }
+
+  function getTime(timeInfo){
+    let time = new Date(timeInfo);
+    let UTCHoursVal = time.getUTCHours() - 1;
+    var hours = UTCHoursVal;
+    let UTCMinutesVal = time.getUTCMinutes();
+    var minutes;
+    var aa;
+    if (UTCHoursVal === 4) {
+        hours = 12;
+        aa = "A.M.";
+    } else if (UTCHoursVal < 4 && UTCHoursVal>= 0){
+      hours = UTCHoursVal-4+12;
+      aa = "P.M.";
+    } else if (UTCHoursVal<16 && UTCHoursVal>=4){
+      hours = UTCHoursVal-4;
+      aa= "A.M.";
+    } else if (UTCHoursVal===16){
+      hours = 12;
+      aa= "P.M.";
+    } else if (UTCHoursVal<24 && UTCHoursVal >= 16) {
+      hours = UTCHoursVal-4-12;
+      aa="P.M.";
+    };
+
+
+    if(UTCMinutesVal < 10){
+      minutes = "0" + UTCMinutesVal;
+    } else {
+      minutes = UTCMinutesVal;
+    }
+    return hours + ":"+minutes + aa;
+  }	
 
 }
 
